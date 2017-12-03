cc51dc143f37dd5d35c2f366591537b1301b8f32
alejgh <alejandrgh11@gmail.com>
2017-12-03 23:10:54 +0100

    Logistic regression and one-vs-all classifier.

--- ex3/lrCostFunction.m
+++ ex3/lrCostFunction.m
@@ -36,8 +36,13 @@ grad = zeros(size(theta));
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
+theta_copy = theta;
+theta_copy(1) = 0;
 
+J = sum(-y .* log(sigmoid(X * theta)) - (1 - y) .* ...
+    log(1 - sigmoid(X * theta))) / m + lambda/(2*m) * sum(theta(2:end).^2);
 
+grad = 1/m * (X' * (sigmoid(X * theta)-y)) + lambda * theta_copy/m;
 
 
 
