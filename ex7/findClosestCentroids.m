b25c07484193ebb50dba57a505ce4db5c46256b7
alejgh <alejandrgh11@gmail.com>
2017-12-31 18:09:59 +0100

    K-means part finished

--- ex7/findClosestCentroids.m
+++ ex7/findClosestCentroids.m
@@ -21,11 +21,11 @@ idx = zeros(size(X,1), 1);
 % Note: You can use a for-loop over the examples to compute this.
 %
 
-
-
-
-
-
+for i = 1:size(X,1)
+    distance = repelem(X(i, :), K, 1) - centroids;
+    distance = sqrt(sum(distance.^2, 2)); %norm
+    [~, idx(i)] = min(distance);
+end
 
 % =============================================================
 
